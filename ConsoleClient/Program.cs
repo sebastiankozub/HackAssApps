146d0299f5eb1284eaed540d6f811a017e40b01b
Sebastian Kozub <sebastiankozub@o2.pl>
2020-09-08 06:48:09 +0100

    init

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -6,6 +6,12 @@ namespace ConsoleClient
     {
         static void Main(string[] args)
         {
+            var i = 0;
+            while (true)
+            {
+                i++;
+                Console.WriteLine("Hello World! {0}", i);
+            }
             Console.WriteLine("Hello World!");
         }
     }
