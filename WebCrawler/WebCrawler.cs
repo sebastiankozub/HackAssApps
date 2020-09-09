40dbce75590d821347828ede8e3cc51cf14ae4d7
Sebastian Kozub <sebastiankozub@o2.pl>
2020-09-09 02:05:03 +0100

    some tries

--- WebCrawler/WebCrawler.cs
+++ WebCrawler/WebCrawler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 
 namespace WebCrawler
 {
@@ -9,4 +10,55 @@ namespace WebCrawler
 
 
     }
+
+
+    public class AttackEngine
+    {
+        private Collection<AttackResult> attackResults;
+        private Collection<IAttackResultCollector> attackResultCollectors;
+
+        public AttackEngine()
+        {
+
+
+        }
+
+
+
+
+    }
+
+
+    public struct AttackResult
+    {
+        public string url;
+        public RequestType requestType;
+        public AttackType attackType;
+        public bool result;
+    }
+
+
+    public enum AttackType
+    { 
+        xss,
+        xsrf,
+    }
+
+    public enum RequestType
+    {
+        get,
+        post,
+    }
+
+    public interface IAttackResult
+    {
+
+
+    }
+
+    public interface IAttackResultCollector
+    {
+
+    }
+
 }
