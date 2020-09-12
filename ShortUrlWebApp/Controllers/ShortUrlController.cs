f239d107eb28d04e9e4d704e16cbe5415ea4386a
Sebastian Kozub <sebastiankozub@o2.pl>
2020-09-12 04:45:00 +0100

    Added shortenUrl web app project init

---
+++ ShortUrlWebApp/Controllers/ShortUrlController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ShortUrlWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShortUrlController : ControllerBase
+    {
+        private readonly ILogger<ShortUrlController> _logger;
+
+        public ShortUrlController(ILogger<ShortUrlController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ShortUrl Get(string url, bool regenrate)
+        {
+            var rng = new Random();
+
+            // finally:
+            // IF (regenrate is false) AND (url is in DB) {return from DB} ESLE {generate new and inseert into DB}
+            // IF (regenrate is true) {generate new and inseert into DB}
+
+            return new ShortUrl
+            {
+                Url = url,
+                UrlEncoded = "endodedUrl" + rng.Next(),
+                Created = DateTime.Now
+            };
+        }
+    }
+}
---
+++ ShortUrlWebApp/ShortUrl.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShortUrlWebApp
+{
+    public class ShortUrl
+    {
+        public string Url { get; set; }
+        public string UrlEncoded { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
